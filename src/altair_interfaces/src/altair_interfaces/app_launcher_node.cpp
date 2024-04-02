9ddce8fe0a2a86ce8536afe7c4967f044cee6484
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-04-02 07:36:02 +0700

    Feat: interfaces and data changes

--- src/altair_interfaces/src/altair_interfaces/app_launcher_node.cpp
+++ src/altair_interfaces/src/altair_interfaces/app_launcher_node.cpp
@@ -5,6 +5,6 @@
 void altair::app_launch() {
     altair::Config config = altair::Config();
 
-    RCLCPP_INFO(rclcpp::get_logger("app_launcher"), "Starting Altair App on %s:3000", config.ip.c_str());
+    RCLCPP_INFO(rclcpp::get_logger("app_launcher"), "Starting Altair App on %s:3000/%s", config.ip.c_str(), config.id.c_str());
     system("node /altair-os/web/app.js");
 }
