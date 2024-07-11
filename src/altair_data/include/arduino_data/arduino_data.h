da5de1c6c6d76462ede39196556c5904643496a3
Lexciese <falah.naufal.zk@gmail.com>
2024-07-11 09:42:04 +0700

    fix: arduino_data can publish imu data & button

--- src/altair_data/include/arduino_data/arduino_data.h
+++ src/altair_data/include/arduino_data/arduino_data.h
@@ -5,93 +5,77 @@
 
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/imu.h"
-#include "std_msgs/msg/string.h"  
+#include "sensor_msgs/msg/imu.hpp"
+#include "std_msgs/msg/char.hpp"
 #include <string>
 #include <string.h>
 #include <yaml-cpp/yaml.h>
 #include <stdio.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/msg/transform_stamped.hpp>
-#include <tf2/convert.h>
-#include <eigen3/Eigen/src/Geometry/Quaternion.h>
-#include "robotis_math/robotis_linear_algebra.h"
+
+#include "robotis_math/robotis_math_base.h"
+using namespace robotis_framework;
+using namespace std;
 
 #include <fcntl.h> // Contains file controls like O_RDWR
 #include <errno.h> // Error integer and strerror() function
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 #include <cstdlib>
-#include <mutex>
 #include <fstream>
 
-class Arduino_Data:
+
+class Arduino_Data : public rclcpp::Node
 {
 
 public:
-  explicit Arduino_Data(int argc, char** argv);
-  ~Arduino_Data();
+  Arduino_Data() : Node("arduino_data_node") 
+  {
+    // get_parameter_or<std::string>("portName", portName, "/dev/ttyUSB0 "); 
+    
+    imu_pub = this->create_publisher<sensor_msgs::msg::Imu>("arduino_data/imu", 1);
+    button_pub = this->create_publisher<std_msgs::msg::Char>("arduino_data/button", 1);
+    process();
 
-  enum rx_arduino_state {
-    Ardu_Start_U,
-    Ardu_Start_G,
-    Ardu_Start_M,
-    Ardu_Start_D,
-    Ardu_Start_I,
-    Ardu_Start_Y,
-    Ardu_Start_P,
-    Ardu_Start_C,
-    Ardu_Start_T,
-    Ardu_Start_B,
-  };
+  }
+  ~Arduino_Data();  
+  
 
-
-signals:
-    void finished();
-    void start();
-
-public slots:
-  void run();
 
 private:
+  sensor_msgs::msg::Imu imu_msg;
+  std_msgs::msg::Char button_msg;
+
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
+  rclcpp::Publisher<std_msgs::msg::Char>::SharedPtr button_pub;
+
   int fd;
-  auto devicedata;
-  std::mutex mutex;
-  bool currentPortNameChanged;
-  std::string portName, currentPortName;
+  const char *portName;
   int baudRate;
   int waitTimeout;
-  int timeOut_ctr;
-  bool broadcast;
-  bool reconnect;
-  bool dev_connected;
-  std::regex regexPattern("[UGMDIYPCTBA]");
+  int timeOut_ctr = 0;
+  bool broadcast = broadcast;
+  bool reconnect = false;
+  bool dev_connected = false;
 
   double offset;
 
   int init_argc_;
   char** init_argv_;
-  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr button_pub;
+  int time;
+  
 
-  bool data_ready;
-  rx_arduino_state arduino_state;
-  double roll,pitch,yaw,gyroRoll,gyroPitch,gyroYaw,accelX,accelY,accelZ, temp_data;
-  std::string read_buffer;
+  bool data_ready = false;
+  float roll, pitch, yaw, gyroRoll, gyroPitch, gyroYaw, accelX, accelY, accelZ, temp_data;
+  string read_buffer;
+  float button;
   char type;
-  int value;
-  int button, prev_button;
-  bool readyToSend;
-  std::map<int, std::string> button_mapping;
+  float value;
 
-  boost::asio::io_service io;
 
-  void debugDevice();
-  void publishData();
+  void configure_port(int fd);
   bool initialize();
-  void deviceCheck();
-  void deviceLoop();
-  void loadButton(const std::string path);
+  void process();
+  
 
 
 };
--- src/altair_data/src/arduino_data/arduino_data.cpp
+++ src/altair_data/src/arduino_data/arduino_data.cpp
@@ -1,112 +1,9 @@
 #include "arduino_data/arduino_data.h"
 
 
-Arduino_Data::Arduino_Data(int argc, char** argv):
-  init_argc_(argc),
-  init_argv_(argv),
-  data_ready(false),
-  reconnect(false),
-  broadcast(false),
-  timeOut_ctr(0),
-  dev_connected(false),
-  readyToSend(false)
-{
-
-  if(!initialize())
-    return;
-}
-
 Arduino_Data::~Arduino_Data()
 {
 
-}
-
-bool Arduino_Data::initialize()
-{
-  RCLCPP_WARN("Initialize Arduino Controller");
-  portName = "ttyACM0";
-  baudRate = 1000000;
-  waitTimeout = 500;
-  broadcast = true;
-  std::string default_path;
-  default_path = "../../config/GlobalConfig.yaml";
-  loadButton(default_path);
-  try {
-    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-  }
-  catch {
-    RCLCPP_ERROR("Error Opening Port");
-    return false;
-  }
-
-  // CONFIGURE PORT
-  struct termios tty;
-  memset(&tty, 0, sizeof tty);
-
-  if (tcgetattr(fd, &tty) != 0) {
-      cerr << "Error " << errno << " from tcgetattr: " << strerror(errno) << endl;
-      return false;
-  }
-
-  cfsetospeed(&tty, B9600);
-  cfsetispeed(&tty, B9600);
-
-  tty.c_cflag &= ~PARENB; // Make 8n1
-  tty.c_cflag &= ~CSTOPB;
-  tty.c_cflag &= ~CSIZE;
-  tty.c_cflag |= CS8;
-
-  tty.c_cflag &= ~CRTSCTS; // No flow control
-  tty.c_cc[VMIN] = 1; // Read at least 1 character
-  tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
-
-  tty.c_cflag |= CREAD | CLOCAL; // Turn on the READ & ignore ctrl lines
-
-  cfmakeraw(&tty);
-
-  tcflush(fd, TCIFLUSH);
-
-  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-      cerr << "Error " << errno << " from tcsetattr" << endl;
-  }
-
-  return true;
-
-}
-
-void Arduino_Data::loadButton(const std::string path)
-{
-  YAML::Node doc;
-  try
-  {
-    // load yaml
-    doc = YAML::LoadFile(path.c_str());
-  } catch (const std::exception& e)
-  {
-    return;
-  }
-
-  // parse button_setting
-  YAML::Node button_mode = doc["button_setting"];
-  for (YAML::iterator yaml_it = button_mode.begin(); yaml_it != button_mode.end(); ++yaml_it)
-  {
-    int index;
-    std::string mode;
-    index = yaml_it->second.as<int>();
-    mode = yaml_it->first.as<std::string>();
-    button_mapping.insert(std::make_pair(index, mode));
-  }
-
-  YAML::Node ardu = doc["Arduino_Data"];
-
-  offset = ardu["offset"].as<double>();
-
-}
-
-void Arduino_Data::debugDevice()
-{
-  RCLCPP_WARN("Baud Rate: %d", baudRate);
-  RCLCPP_WARN("Port Name: %s", portName);
 }
 
 void Arduino_Data::configure_port(int fd)
@@ -119,8 +16,8 @@ void Arduino_Data::configure_port(int fd)
         return;
     }
 
-    cfsetospeed(&tty, B1000000);
-    cfsetispeed(&tty, B1000000);
+    cfsetospeed(&tty, B9600);
+    cfsetispeed(&tty, B9600);
 
     tty.c_cflag &= ~PARENB; // Make 8n1
     tty.c_cflag &= ~CSTOPB;
@@ -142,181 +39,123 @@ void Arduino_Data::configure_port(int fd)
     }
 }
 
-void Arduino_Data::run(std::promise<void>& quitSignal)
+bool Arduino_Data::initialize()
 {
-
-  while (rclcpp::ok())
-  {
-    deviceLoop();
-    if(data_ready)
-      publishData();
-    rclcpp::spinOnce(ros_node);
+  RCLCPP_WARN(rclcpp::get_logger("arduino_data_node"), "Initialize Arduino Controller");
+  portName = "/dev/ttyUSB0";
+  baudRate = 9600;
+  waitTimeout = 500;
+  broadcast = true;
+  std::string default_path;
+  default_path = "../../config/GlobalConfig.yaml";
+  // loadButton(default_path);
+  try {
+    fd = open(portName, O_RDWR | O_NOCTTY | O_SYNC);
   }
-  // close(fd);
+  catch (const std::exception& e){
+    RCLCPP_ERROR(rclcpp::get_logger("arduino_data_node"), "Error Opening Port");
+    return false;
+  }
 
-  RCLCPP_WARN("Ros shutdown, proceeding to close the port");
-  // Signal that the task is finished
-  // quitSignal.set_value();
-  // std::exit(0);
+  // CONFIGURE PORT
+  Arduino_Data::configure_port(fd);
+
+  return true;
+
 }
 
-void Arduino_Data::deviceLoop()
+
+void Arduino_Data::process()
 {
-  // fd = open(portName, O_RDWR | O_NOCTTY | O_SYNC);
-  // if (fd < 0) {
-  //   cerr << "Error " << errno << " opening " << portName << ": " << strerror(errno) << endl;
-  //   return;
-  // }
+  Arduino_Data::initialize();
   if(reconnect) {
     close(fd);
-    Arduino_Data::initialize();
+    if(Arduino_Data::initialize()) {
+      reconnect = false;
+    }
   }
-  
   std::ifstream serial(portName);
 
-  // removed while(getline(...)) {}
-  std::getline(serial, read_buffer)
-  std::istringstream iss(read_buffer);
-  char type;
-  double value;
-  dev_connected = true;
+  while (getline(serial, read_buffer) && rclcpp::ok()) {
+    
+    istringstream iss(read_buffer);
+    
 
-  if (iss >> type >> value) {
-    switch (type) {
-      case 'U':
-          // std::cout << "Type U: " << value << '\n';
-          roll = value;
-          data_ready = false;
-          break;
-      case 'G':
-          // std::cout << "Type G: " << value << '\n';
-          pitch = value;
-          data_ready = false;
-          break;
-      case 'M':
-          // std::cout << "Type M: " << value << '\n';
-          yaw = value;
-          data_ready = false;
-          break;
-      case 'D':
-          // std::cout << "Type D: " << value << '\n';
-          gyroRoll = value;
-          data_ready = false;
-          break;
-      case 'I':
-          // std::cout << "Type I: " << value << '\n';
-          gyroPitch = value;
-          data_ready = false;
-          break;
-      case 'Y':
-          // std::cout << "Type Y: " << value << '\n';
-          gyroYaw = value;
-          data_ready = false;
-          break;
-      case 'P':
-          // std::cout << "Type P: " << value << '\n';
-          accelX = value;
-          data_ready = false;
-          break;
-      case 'C':
-          // std::cout << "Type C: " << value << '\n';
-          accelY = value;
-          data_ready = false;
-          break;
-      case 'T':
-          // std::cout << "Type T: " << value << '\n';
-          accelZ = value;
-          data_ready = true;
-          break;
-      case 'B':
-          // std::cout << "Type B: " << value << '\n';
-          button = value;
-          data_ready = true;
-          break;
+    if (data_ready) {
+      imu_msg.header.frame_id = "imu_link";
+      imu_msg.header.stamp = rclcpp::Clock().now();
+      imu_pub->publish(imu_msg);
+    }
+
+    if (iss >> type >> value) {
+      switch (type) {
+          case 'U':
+              cout << "Type U: " << value << '\n';
+              roll = -1 * value * DEGREE2RADIAN;
+              imu_msg.orientation.x = roll;
+              data_ready = false;
+              break;
+          case 'G':
+              cout << "Type G: " << value << '\n';
+              pitch = value * DEGREE2RADIAN;
+              imu_msg.orientation.y = pitch;
+              data_ready = false;
+              break;
+          case 'M':
+              cout << "Type M: " << value << '\n';
+              yaw = -1 * value * DEGREE2RADIAN;
+              imu_msg.orientation.z = yaw;
+              data_ready = false;
+              break;
+          case 'D':
+              cout << "Type D: " << value << '\n';
+              gyroRoll = value * DEGREE2RADIAN;
+              imu_msg.angular_velocity.x = gyroRoll;
+              data_ready = false;
+              break;
+          case 'I':
+              cout << "Type I: " << value << '\n';
+              gyroPitch = -1 * value * DEGREE2RADIAN;
+              imu_msg.angular_velocity.y = gyroPitch;
+              data_ready = false;
+              break;
+          case 'Y':
+              cout << "Type Y: " << value << '\n';
+              gyroYaw = value * DEGREE2RADIAN;
+              imu_msg.angular_velocity.z = gyroYaw;
+              data_ready = false;
+              break;
+          case 'P':
+              cout << "Type P: " << value << '\n';
+              accelX = value;
+              imu_msg.linear_acceleration.x = accelX;
+              data_ready = false;
+              break;
+          case 'C':
+              cout << "Type C: " << value << '\n';
+              accelY = value;
+              imu_msg.linear_acceleration.y = accelY;
+              data_ready = false;
+              break;
+          case 'T':
+              cout << "Type T: " << value << '\n';
+              accelZ = value;
+              imu_msg.linear_acceleration.z = accelZ;
+              data_ready = true;
+              break;
+          case 'B':
+              cout << "Type B: " << value << '\n';
+              button_msg.data = (char)value;
+              button_pub->publish(button_msg);
+              // data_ready = true;
+              break;
+      }
+    }
+    else {
+      reconnect = true;
+      RCLCPP_ERROR(rclcpp::get_logger("arduino_data_node"), "Error Parsing Data. Trying to reconnect Now... \nIf the error persist, try to reconnect USB or restart arduino_data_node");
     }
   }
-  else {
-    // std::cerr << "Failed to parse line: " << line << '\n';
-    RCLCPP_ERROR("Failed to Parse Data");
-    reconnect = true;
-    dev_connected = false;
-    return;
-  }
 }
 
-void Arduino_Data::publishData()
-{
-  double roll_ = -roll * DEGREE2RADIAN; double pitch_ = pitch * DEGREE2RADIAN; double yaw_ = -yaw * DEGREE2RADIAN;
-  double gyroRoll_ = gyroRoll * DEGREE2RADIAN; double gyroPitch_ = -gyroPitch * DEGREE2RADIAN; double gyroYaw_ = gyroYaw * DEGREE2RADIAN;
-  double accelX_ = accelX; double accelY_ = accelY; double accelZ_ = accelZ;
 
-  if(!dev_connected)
-  {
-    roll_ = pitch_ = yaw_ = gyroRoll_ = gyroPitch_ = gyroYaw_ = 0;
-  }
-
-
-  Eigen::Quaterniond quaternion = robotis_framework::convertRPYToQuaternion(roll_, pitch_, yaw_);
-  Eigen::Vector3 gyro(gyroRoll_, gyroPitch_, gyroYaw_);
-  Eigen::Vector3 accel(accelX_, accelY_, accelZ_);
-  sensor_msgs::msg::Imu imu;
-
-  // DEPRECATED CODES
-  // tf2::StampedTransform transform;
-  // tf2::TransformBroadcaster br;
-  // tf2::quaternionEigenToMsg(quaternion, imu.orientation);
-  // tf2::vectorEigenToMsg(gyro, imu.angular_velocity);
-  // tf2::vectorEigenToMsg(accel, imu.linear_acceleration);
-
-  geometry_msgs::msg::TransformStamped transform;
-  tf2_ros::TransformBroadcaster br;
-  tf2::convert(quaternion, imu_msg.orientation);
-  tf2::convert(gyro, imu_msg.angular_velocity);
-  tf2::convert(accel, imu_msg.linear_acceleration);
-
-
-  imu.header.stamp = rclcpp::Time::now();
-  imu.header.frame_id = "imu_link";
-
-  imu_pub->publish(imu);
-
-  std_msgs::msg::String button_str;
-
-  if(button != prev_button)
-  {
-    std::map<int, std::string>::iterator iter = button_mapping.find(button);
-    if (iter != button_mapping.end())
-    {
-      std::string data_ = iter->second;
-      button_str.data = data_;
-    }
-    else button_str.data = std::string("None");
-
-  readyToSend = true;
-
-  }
-
-  prev_button = button;
-
-  if(readyToSend)
-{
-readyToSend = false;
-button_pub->publish(button_str);
-}
-  if(!dev_connected || reconnect)
-    button_str.data = std::string("None");
-
-  //check bentuk imu di rviz
-  if(broadcast)
-  {
-    transform.frame_id_= "base_link";
-    transform.child_frame_id_ = "imu_link";
-    transform.stamp_ = rclcpp::Time::now();
-    transform.setOrigin(tf2::Vector3(0,0,0));
-    tf2::Quaternion q = tf2::createQuaternionFromRPY(roll_, pitch_, yaw_);
-    transform.setRotation(q);
-    br.sendTransform(transform);
-  }
-
-
-}
