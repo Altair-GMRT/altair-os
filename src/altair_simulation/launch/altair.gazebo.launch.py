0a711b33babdfa8e00aec76d4333a77d124a6f1f
Lexciese <falah.naufal.zk@gmail.com>
2024-08-05 11:04:29 +0700

    update texture

--- src/altair_simulation/launch/altair.gazebo.launch.py
+++ src/altair_simulation/launch/altair.gazebo.launch.py
@@ -20,27 +20,91 @@ def generate_launch_description():
         'worlds',
         'altair_world.sdf'
     )
+    spawn_world = ExecuteProcess(
+            cmd=['gz', 'sim', '-v','-r', gazebo_world_path],
+            output='screen'
+        )
+    
+    topic_brige_path = os.path.join(
+    get_package_share_directory('altair_simulation'),
+    'config',
+    'bridge.yaml'
+    )
+    topic_brige = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={topic_brige_path}',
+        ],
+        output='screen',
+    )
+    camera_bridge = Node(
+    package='ros_gz_image',
+    executable='image_bridge',
+    arguments=['gazebo/camera/image_raw'],
+    output='screen',
+)
 
     # Path to the robot model file
     # already added in the altair_world.sdf file
-    # robot_model_path = os.path.join(
-    #     get_package_share_directory('altair_simulation'),
-    #     'urdf',
-    #     'robot.urdf'
-    # )
+    robot_model_path = os.path.join(
+        get_package_share_directory('altair_simulation'),
+        'urdf',
+        'robot.urdf'
+    )
+    with open(robot_model_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_urdf_path = os.path.join(
+        get_package_share_directory('altair_simulation'),
+        'urdf',
+        'robot.urdf'
+    )
+    robot_xacro_path = os.path.join(
+        get_package_share_directory('altair_simulation'),
+        'urdf',
+        'robotis_op3.urdf.xacro'
+    )
+
+    convert_urdf_xacro = ExecuteProcess(
+        cmd=['xacro', robot_xacro_path, '-o', robot_urdf_path],
+        output='screen'
+    )
+
+    spawn_robot = ExecuteProcess(
+        cmd=["ros2 launch ros_gz_sim gz_spawn_model.launch.py file:=$(ros2 pkg prefix --share altair_simulation)/urdf/robot.urdf name:=robot"],
+        output='screen'
+    )
+
+    # joint_state_publisher_gui
+    joint_state_publisher_gui = Node(
+     package='joint_state_publisher_gui',
+     executable='joint_state_publisher_gui',
+     name='joint_state_publisher_gui',
+     arguments=[robot_model_path],
+     output=['screen']
+    )
+    robot_state_publisher = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    name='robot_state_publisher',
+    output='both',
+    parameters=[
+        {'use_sim_time': True},
+        {'robot_description': robot_desc},
+    ]
+)
 
     return LaunchDescription([
         # Launch Gazebo simulator
-        ExecuteProcess(
-            cmd=['gz', 'sim', '-r', gazebo_world_path],
-            output='screen'
-        ),
-        
-        # Spawn a robot model
-        # Node(
-        #     package='ros_gz_sim',
-        #     executable='create',
-        #     arguments=['-name', 'my_robot', '-file', robot_model_path],
-        #     output='screen'
-        # )
+        spawn_world,
+        topic_brige,
+        camera_bridge,
+        # convert_urdf_xacro,
+        # spawn_robot,
+        joint_state_publisher_gui,
+        robot_state_publisher
+
     ])
